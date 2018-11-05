36cec9506d36ffb4f87633e7be53ac761d3c4d55
luojiannan <399129811@qq.com>
2018-11-05 14:04:37 +0800

    用户dao

---
+++ pinyougou-user/src/main/java/com/pinyougou/dao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.pinyougou.dao.mapper;
+
+import com.pinyougou.dao.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author ljn
+ */
+
+public interface UserMapper  extends Mapper<User>{
+}
