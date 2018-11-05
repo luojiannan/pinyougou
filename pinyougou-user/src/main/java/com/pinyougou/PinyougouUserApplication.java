50ff3e73fc0a07cc20cc752a7d66b744f37aa5d8
luojiannan <399129811@qq.com>
2018-11-05 15:03:46 +0800

    数据库连接

--- pinyougou-user/src/main/java/com/pinyougou/PinyougouUserApplication.java
+++ pinyougou-user/src/main/java/com/pinyougou/PinyougouUserApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableEurekaClient
+@MapperScan(basePackages = "com.pinyougou.dao.mapper")
 public class PinyougouUserApplication {
 
 	public static void main(String[] args) {
--- pinyougou-user/src/main/java/com/pinyougou/controller/UserController.java
+++ pinyougou-user/src/main/java/com/pinyougou/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.pinyougou.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.pinyougou.dao.entity.User;
+import com.pinyougou.dao.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author ljn
@@ -12,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("getUser")
     public String getUser(){
-        return "bruce";
+        List<User> users = userMapper.selectAll();
+        String json = JSONObject.toJSONString(users);
+        return json;
     }
 }
