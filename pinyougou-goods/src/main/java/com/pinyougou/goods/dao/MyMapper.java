7ddc5f2299d37edda0af53e0d07e76f4a4c12c43
luojiannan <399129811@qq.com>
2018-11-28 17:02:51 +0800

    增加通用mapper

---
+++ pinyougou-goods/src/main/java/com/pinyougou/goods/dao/MyMapper.java
@@ -0,0 +1,11 @@
+package com.pinyougou.goods.dao;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * @author ljn
+ * @date 2018/11/28.
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T>{
+}
--- pinyougou-goods/src/main/java/com/pinyougou/goods/dao/mapper/SpecificationMapper.java
+++ pinyougou-goods/src/main/java/com/pinyougou/goods/dao/mapper/SpecificationMapper.java
@@ -1,13 +1,13 @@
 package com.pinyougou.goods.dao.mapper;
 
+import com.pinyougou.goods.dao.MyMapper;
 import com.pinyougou.goods.dao.entity.Specification;
 import org.springframework.stereotype.Component;
-import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author ljn
  */
 
 @Component
-public interface SpecificationMapper extends Mapper<Specification> {
+public interface SpecificationMapper extends MyMapper<Specification> {
 }
