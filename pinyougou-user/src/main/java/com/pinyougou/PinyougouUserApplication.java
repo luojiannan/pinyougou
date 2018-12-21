44f5441d20bfa9bde4eca5d70e3ea064316df45f
luojiannan <399129811@qq.com>
2018-12-21 15:17:14 +0800

    用户，角色管理

--- pinyougou-user/src/main/java/com/pinyougou/PinyougouUserApplication.java
+++ pinyougou-user/src/main/java/com/pinyougou/PinyougouUserApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableEurekaClient
 @MapperScan(basePackages = "com.pinyougou.user.dao.mapper")
+@EnableSwagger2
 public class PinyougouUserApplication {
 
 	public static void main(String[] args) {
