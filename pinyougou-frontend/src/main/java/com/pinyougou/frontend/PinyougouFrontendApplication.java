5788ce0b089c21a32d32596b546aee81ae859e64
luojiannan <399129811@qq.com>
2019-02-15 15:16:36 +0800

    swagger依赖

--- pinyougou-frontend/src/main/java/com/pinyougou/frontend/PinyougouFrontendApplication.java
+++ pinyougou-frontend/src/main/java/com/pinyougou/frontend/PinyougouFrontendApplication.java
@@ -2,6 +2,8 @@ package com.pinyougou.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableFeignClients
 @EnableSwagger2
