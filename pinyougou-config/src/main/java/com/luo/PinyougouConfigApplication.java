2dbbd5cc9e484db9886467440aa723f8e252352d
luojiannan <399129811@qq.com>
2019-07-10 16:21:56 +0800

    spring cloud config

--- pinyougou-config/src/main/java/com/luo/PinyougouConfigApplication.java
+++ pinyougou-config/src/main/java/com/luo/PinyougouConfigApplication.java
@@ -2,8 +2,10 @@ package com.luo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PinyougouConfigApplication {
 
 	public static void main(String[] args) {
